23bd603f6b881df93541a1666cb2d4a6698380b2
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-08 00:07:01 +0900

    Product 수정

--- src/main/java/hhplus/ecommerce/product/domain/Product.java
+++ src/main/java/hhplus/ecommerce/product/domain/Product.java
@@ -1,24 +1,17 @@
 package hhplus.ecommerce.product.domain;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Getter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public record Product(
+public record Product (
         Long productId,
-        String name,
-        int price,
-        int stock,
-        String size,
-        String color
 
+        String name,
+
+        int price,
+
+        int stock,
+
+        String size,
+
+        String color
 ) {
 
 }
