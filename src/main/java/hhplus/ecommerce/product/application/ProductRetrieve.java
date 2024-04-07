8db18fb3400b68830fb3ebe7e3e78f1eb5bd3cae
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-07 21:28:23 +0900

    ProductRetrieve 수정

---
+++ src/main/java/hhplus/ecommerce/product/application/ProductRetrieve.java
@@ -0,0 +1,26 @@
+package hhplus.ecommerce.product.application;
+
+
+import hhplus.ecommerce.product.domain.Product;
+import hhplus.ecommerce.product.infrastructure.ProductRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class ProductRetrieve {
+    private final ProductRepository productRepository;
+
+    public ProductRetrieve(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> readAll() {
+        return productRepository.findAll();
+    }
+
+    public Product readById(Long produtId) {
+        return productRepository.findById(produtId);
+    }
+}
