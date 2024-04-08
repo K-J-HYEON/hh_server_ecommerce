734d587e103151f5e2c8eae6660016c695d48b44
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-09 02:25:18 +0900

    Swagger 반영

--- src/main/java/hhplus/ecommerce/product/presentation/ProductController.java
+++ src/main/java/hhplus/ecommerce/product/presentation/ProductController.java
@@ -5,9 +5,17 @@ import hhplus.ecommerce.product.domain.Product;
 import hhplus.ecommerce.product.dto.res.ProductInfoDetailRes;
 import hhplus.ecommerce.product.dto.res.ProductInfoRes;
 import hhplus.ecommerce.product.dto.res.ProductListRes;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+
+@Tag(name = "상품 조회", description = "상품 조회 관련 api 입니다.")
 @RestController
 @RequestMapping("/ecommerce/api/product")
 public class ProductController {
@@ -18,6 +26,12 @@ public class ProductController {
         this.productService = productService;
     }
 
+
+    @Operation(summary = "상품 조회 메서드", description = "상품 조회 메서드입니다.", tags = "상품")	// (2)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = ProductInfoRes.class))),
+            @ApiResponse(responseCode = "400", description = "bad request operation", content = @Content(schema = @Schema(implementation = ProductInfoRes.class)))
+    })
     @GetMapping
     public ProductListRes readProductInfo() {
         List<Product> products = productService.readProductInfo();
@@ -28,6 +42,11 @@ public class ProductController {
         return new ProductListRes(productInfoResList);
     }
 
+    @Operation(summary = "상품 상세 조회 메서드", description = "상품 상세 조회 메서드입니다.", tags = "상품")	// (2)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = ProductInfoDetailRes.class))),
+            @ApiResponse(responseCode = "400", description = "bad request operation", content = @Content(schema = @Schema(implementation = ProductInfoDetailRes.class)))
+    })
     @GetMapping("/{productId}")
     public ProductInfoDetailRes readProductInfoDetail(@PathVariable Long productId) {
         Product product = productService.readProductInfoDetail(productId);
