4801a5c11f603de6135290e9b551a8dd31339dd8
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-10 21:12:35 +0900

    ProductRepository 반영

--- src/main/java/hhplus/ecommerce/product/infrastructure/ProductRepository.java
+++ src/main/java/hhplus/ecommerce/product/infrastructure/ProductRepository.java
@@ -1,13 +1,23 @@
 package hhplus.ecommerce.product.infrastructure;
 
+import hhplus.ecommerce.order.util.OrderStatus;
 import hhplus.ecommerce.product.domain.Product;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Pageable;
+import java.time.LocalDateTime;
 import java.util.List;
 
-@Repository
 public interface ProductRepository {
 
     List<Product> findAll();
 
     Product findById(Long productId);
+
+    List<Product> findByIdIn(List<Long> productIds);
+
+    void updateStock(Product product);
+
+    List<Product> readPopularSellingProducts(OrderStatus orderStatus,
+                                         LocalDateTime startDate,
+                                         LocalDateTime endDate,
+                                         Pageable pageable);
 }
