27cc4bfdd521bb4ea712882d2147b093f5cd6401
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-10 21:05:30 +0900

    OrderUpdater 반영

---
+++ src/main/java/hhplus/ecommerce/order/util/OrderUpdater.java
@@ -0,0 +1,19 @@
+package hhplus.ecommerce.order.util;
+
+
+import hhplus.ecommerce.order.domain.Order;
+import hhplus.ecommerce.order.infrastructure.OrderRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderUpdater {
+    private final OrderRepository orderRepository;
+
+    public OrderUpdater(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public Order changeStatus(Order order, OrderStatus orderStatus) {
+        return orderRepository.updateStatus(order, orderStatus);
+    }
+}
