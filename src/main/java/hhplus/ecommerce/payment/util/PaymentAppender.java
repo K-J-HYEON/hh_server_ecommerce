2d29dcc4cecae72aeff4b019f02e4770309af8e6
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-10 21:07:54 +0900

    PaymentAppender 반영

---
+++ src/main/java/hhplus/ecommerce/payment/util/PaymentAppender.java
@@ -0,0 +1,21 @@
+package hhplus.ecommerce.payment.util;
+
+
+import hhplus.ecommerce.order.domain.Order;
+import hhplus.ecommerce.payment.domain.Payment;
+import hhplus.ecommerce.payment.infrastructure.PaymentRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentAppender {
+
+    private final PaymentRepository paymentRepository;
+
+    public PaymentAppender(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
+
+    public Payment create(Order order, Long payAmount, String paymentMethod) {
+        return paymentRepository.create(order.orderId(), payAmount, paymentMethod);
+    }
+}
