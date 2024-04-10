8a7a08243ebe13bc88bcaa6e6b61769044767dc3
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-10 21:12:25 +0900

    ProductKeyRepository 반영

--- src/main/java/hhplus/ecommerce/product/infrastructure/ProductKeyRepository.java
+++ src/main/java/hhplus/ecommerce/product/infrastructure/ProductKeyRepository.java
@@ -1,9 +1,12 @@
 package hhplus.ecommerce.product.infrastructure;
 
+import hhplus.ecommerce.order.util.OrderStatus;
 import hhplus.ecommerce.product.domain.Product;
 import hhplus.ecommerce.product.entity.ProductEntity;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
+import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.List;
 
@@ -29,5 +32,26 @@ public class ProductKeyRepository implements ProductRepository {
         return productJpaRepository.findById(productId)
                 .orElseThrow(() -> new EntityNotFoundException("상품 정보를 찾지 못했습니다. - id: " + productId))
                 .toProduct();
+    }
+
+    @Override
+    public List<Product> findByIdIn(List<Long> productIds) {
+        return productJpaRepository.findByIdIn(productIds).stream()
+                .map(ProductEntity::toProduct)
+                .toList();
+    }
+
+    @Override
+    public void updateStock(Product product) {
+        ProductEntity productEntity = productJpaRepository.findById(product.productId())
+                .orElseThrow(() -> new EntityNotFoundException("상품 정보를 찾지 못하였습니다. - id: " + product.productId()));
+        productEntity.updateStock(product.stockCount());
+    }
+
+    @Override
+    public List<Product> readPopularSellingProducts(OrderStatus orderStatus, LocalDateTime startDate, LocalDateTime endDate, Pageable pageable) {
+        return productJpaRepository.readBestSellingProducts(orderStatus, startDate, endDate, pageable)
+                .map(ProductEntity::toProduct)
+                .toList();
     }
 }
