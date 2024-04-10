13bcfe5dba5d480eb6b80096affc0fa5c7911247
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-10 20:54:40 +0900

    OrderKeyRepository 반영

---
+++ src/main/java/hhplus/ecommerce/order/infrastructure/OrderKeyRepository.java
@@ -0,0 +1,53 @@
+package hhplus.ecommerce.order.infrastructure;
+
+
+import hhplus.ecommerce.order.domain.Order;
+import hhplus.ecommerce.order.dto.request.OrderReq;
+import hhplus.ecommerce.order.dto.request.Receiver;
+import hhplus.ecommerce.order.entity.OrderEntity;
+import hhplus.ecommerce.order.util.OrderStatus;
+import hhplus.ecommerce.user.domain.User;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+
+@Repository
+public class OrderKeyRepository implements OrderRepository {
+
+    private final OrderJpaRepository orderJpaRepository;
+
+    public OrderKeyRepository(OrderJpaRepository orderJpaRepository) {
+        this.orderJpaRepository = orderJpaRepository;
+    }
+
+    @Override
+    public Order order(User user, OrderReq req) {
+        Receiver receiver = req.receiver();
+        OrderEntity orderEntity = new OrderEntity(
+                user.id(),
+                req.paymentAmount(),
+                receiver.name(),
+                receiver.address(),
+                receiver.phoneNunber(),
+                OrderStatus.READY,
+                LocalDateTime.now()
+        );
+        return orderJpaRepository.save(orderEntity).toOrder();
+    }
+
+    @Override
+    public Order updateStatus(Order order, OrderStatus orderStatus) {
+        OrderEntity orderEntity = orderJpaRepository.findById(order.orderId())
+                .orElseThrow();
+        orderEntity.updateStatus(orderStatus);
+        return orderJpaRepository.save(orderEntity).toOrder();
+    }
+
+    @Override
+    public Order findById(Long orderId) {
+        return orderJpaRepository.findById(orderId)
+                .orElseThrow(() -> new EntityNotFoundException("주문정보를 찾지 못하였습니다. - id: " + orderId))
+                .toOrder();
+    }
+}
