0b9a261ef0d6534669623b89b66a337df06fb551
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-10 21:05:22 +0900

    OrderStatus 반영

---
+++ src/main/java/hhplus/ecommerce/order/util/OrderStatus.java
@@ -0,0 +1,30 @@
+package hhplus.ecommerce.order.util;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+public enum OrderStatus {
+    READY("ready"),
+    PAID("paid"),
+    PAY_FAILED("pay failed"),
+    COMPLETE("complete"),
+    CANCELED("canceled");
+
+    private final String value;
+
+    OrderStatus(String value) {
+        this.value = value;
+    }
+
+    public String toString() {
+        return value;
+    }
+
+    public static OrderStatus of(String value) {
+        return Arrays.stream(OrderStatus.values())
+                .filter(orderStatus -> orderStatus.value.equals(value))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("주문상태가 존재하지 않습니다."));
+    }
+}
