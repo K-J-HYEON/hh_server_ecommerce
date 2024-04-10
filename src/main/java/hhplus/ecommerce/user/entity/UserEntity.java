b47b9faf10da8a91c19a728dc5fa0aea0e09fbc8
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-11 00:51:13 +0900

    UserEntity 수정

--- src/main/java/hhplus/ecommerce/user/entity/UserEntity.java
+++ src/main/java/hhplus/ecommerce/user/entity/UserEntity.java
@@ -1,37 +1,39 @@
 package hhplus.ecommerce.user.entity;
 
+import hhplus.ecommerce.config.BaseTimeEntity;
 import hhplus.ecommerce.user.domain.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-
 @Table(name = "User")
-@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class UserEntity {
+@Getter
+public class UserEntity extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
-
     @Column(name = "name")
     private String name;
 
     @Column(name = "address")
     private String address;
 
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
     @Column(name = "point")
     private Long point;
 
-    public User toUser() {
-        return new User(getId(), name, address, point);
+    public Long getId() {
+        return userId;
     }
 
-    private Long getId() {
-        return null;
+    public User toUser() {
+        return new User(getId(), name, address, phoneNumber, point);
     }
 
     public void updatePoint(User user) {
