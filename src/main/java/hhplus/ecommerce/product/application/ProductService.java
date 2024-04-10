d439d77cac756fa27be78bed6edd12f7d67dc7bc
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-11 01:56:12 +0900

    ProductService 수정

--- src/main/java/hhplus/ecommerce/product/application/ProductService.java
+++ src/main/java/hhplus/ecommerce/product/application/ProductService.java
@@ -1,11 +1,7 @@
 package hhplus.ecommerce.product.application;
 
-import hhplus.ecommerce.config.BaseException;
 import hhplus.ecommerce.product.domain.Product;
-import hhplus.ecommerce.product.dto.res.ProductInfoRes;
-
 import java.util.List;
-import java.util.Optional;
 
 public interface ProductService {
 
