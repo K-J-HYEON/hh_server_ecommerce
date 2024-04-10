5d5fa9cddf8fadbbaead8691d9409c6fa33e76f5
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-10 20:54:17 +0900

    OrderItemRepository 반영

---
+++ src/main/java/hhplus/ecommerce/orderitem/infrastructure/OrderItemRepository.java
@@ -0,0 +1,11 @@
+package hhplus.ecommerce.orderitem.infrastructure;
+
+import hhplus.ecommerce.orderitem.Entity.OrderItemEntity;
+import hhplus.ecommerce.orderitem.application.OrderItemAppender;
+import hhplus.ecommerce.orderitem.domain.OrderItem;
+
+import java.util.List;
+
+public interface OrderItemRepository {
+    List<OrderItem> createOrderItem(List<OrderItemEntity> orderItemEntities);
+}
