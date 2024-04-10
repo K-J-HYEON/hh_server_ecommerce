423b3285eeb7cfef944951314a9464967e01eda0
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-10 21:11:07 +0900

    Product 반영

--- src/main/java/hhplus/ecommerce/product/domain/Product.java
+++ src/main/java/hhplus/ecommerce/product/domain/Product.java
@@ -2,16 +2,22 @@ package hhplus.ecommerce.product.domain;
 
 public record Product (
         Long productId,
-
         String name,
-
-        int price,
-
-        int stock,
-
+        Long price,
+        Long stockCount,
         String size,
-
         String color
 ) {
 
+    public Long orderTotalPrice(Long count) {
+        return price * count;
+    }
+
+    public boolean isExistProductStock(Long orderCount) {
+        return stockCount >= orderCount;
+    }
+
+    public Product decreaseStock(Long count) {
+        return new Product(productId, name, price, stockCount - count, size, color);
+    }
 }
