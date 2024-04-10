2dcd5c3bc45daf9db99876cfcc6d188ee0d8a91f
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-10 21:15:09 +0900

    UserPointValidator 반영

---
+++ src/main/java/hhplus/ecommerce/user/util/UserPointValidator.java
@@ -0,0 +1,23 @@
+package hhplus.ecommerce.user.util;
+
+import hhplus.ecommerce.order.domain.Order;
+import hhplus.ecommerce.order.util.OrderStatus;
+import hhplus.ecommerce.order.util.OrderUpdater;
+import hhplus.ecommerce.user.domain.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserPointValidator {
+    private final OrderUpdater orderUpdater;
+
+    public UserPointValidator(OrderUpdater orderUpdater) {
+        this.orderUpdater = orderUpdater;
+    }
+
+    public void confirmUserPoint(Order order, User user, Long payAmount) {
+        if (user.point() < payAmount) {
+            orderUpdater.changeStatus(order, OrderStatus.PAY_FAILED);
+            throw new IllegalArgumentException("현재 잔액은 " + user.point() + "이고 잔액이 부족합니다.");
+        }
+    }
+}
