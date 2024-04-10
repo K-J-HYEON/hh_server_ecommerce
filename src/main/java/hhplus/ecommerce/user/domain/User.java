5f9a11a02a04d42b348dc5546970f379081d40a2
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-10 21:14:36 +0900

    User 반영

--- src/main/java/hhplus/ecommerce/user/domain/User.java
+++ src/main/java/hhplus/ecommerce/user/domain/User.java
@@ -4,9 +4,15 @@ public record User (
     Long id,
     String name,
     String address,
+    String phoneNumber,
     Long point
-) {
+) { ;
+
     public User addPoint(Long chargePoint) {
-        return new User(id, name, address, point + chargePoint);
+        return new User(id, name, address, phoneNumber, point + chargePoint);
+    }
+
+    public User decreasePoint(Long amount) {
+        return new User(id, name, address, phoneNumber, point - amount);
     }
 }
