6edbe1c54d05e2ae35917b269bc65eb93c69edbe
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-10 21:13:14 +0900

    ProductServiceImpl 반영

--- src/main/java/hhplus/ecommerce/product/application/ProductServiceImpl.java
+++ src/main/java/hhplus/ecommerce/product/application/ProductServiceImpl.java
@@ -6,24 +6,26 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
-@RequiredArgsConstructor
 @Service
-@Slf4j
 public class ProductServiceImpl implements ProductService {
 
     private final ProductRetrieve productRetrieve;
-
     public ProductServiceImpl(ProductRetrieve productRetrieve) {
         this.productRetrieve = productRetrieve;
     }
 
     @Override
     public List<Product> readProductInfo() {
-        return productRetrieve.readAll();
+        return productRetrieve.retrieveAll();
     }
 
     @Override
     public Product readProductInfoDetail(Long productId) {
-        return productRetrieve.readById(productId);
+        return productRetrieve.retrieveById(productId);
+    }
+
+    @Override
+    public List<Product> readPopularProduct() {
+        return productRetrieve.readPopularSellingProducts();
     }
 }
