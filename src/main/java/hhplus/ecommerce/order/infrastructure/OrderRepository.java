f9d5dcb65b9209ac1e422fb873ddeb2a0224b774
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-10 20:54:54 +0900

    OrderRepository 반영

---
+++ src/main/java/hhplus/ecommerce/order/infrastructure/OrderRepository.java
@@ -0,0 +1,14 @@
+package hhplus.ecommerce.order.infrastructure;
+
+import hhplus.ecommerce.order.domain.Order;
+import hhplus.ecommerce.order.dto.request.OrderReq;
+import hhplus.ecommerce.order.util.OrderStatus;
+import hhplus.ecommerce.user.domain.User;
+
+public interface OrderRepository {
+    Order order(User user, OrderReq req);
+
+    Order updateStatus(Order order, OrderStatus orderStatus);
+
+    Order findById(Long orderId);
+}
