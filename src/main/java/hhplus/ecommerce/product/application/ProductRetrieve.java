98ae939e8b1e566b5be0048ce163a608de6ebed4
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-10 21:12:46 +0900

    ProductRetrieve 반영

--- src/main/java/hhplus/ecommerce/product/application/ProductRetrieve.java
+++ src/main/java/hhplus/ecommerce/product/application/ProductRetrieve.java
@@ -1,9 +1,14 @@
 package hhplus.ecommerce.product.application;
 
-
+import hhplus.ecommerce.order.dto.request.OrderReq;
+import hhplus.ecommerce.order.util.OrderStatus;
 import hhplus.ecommerce.product.domain.Product;
 import hhplus.ecommerce.product.infrastructure.ProductRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Component
@@ -14,11 +19,26 @@ public class ProductRetrieve {
         this.productRepository = productRepository;
     }
 
-    public List<Product> readAll() {
+    public List<Product> retrieveAll() {
         return productRepository.findAll();
     }
 
-    public Product readById(Long produtId) {
+    public Product retrieveById(Long produtId) {
         return productRepository.findById(produtId);
+    }
+
+    public List<Product> retrieveAllByIds(List<OrderReq.ProductOrderReq> products) {
+        return productRepository.findByIdIn(products.stream()
+                .map(OrderReq.ProductOrderReq::id)
+                .toList()
+        );
+    }
+
+    public List<Product> readPopularSellingProducts() {
+        LocalDateTime endDate = LocalDateTime.now();
+        LocalDateTime startDate = endDate.minusDays(3);
+        Pageable topFive = PageRequest.of(0, 5);
+
+        return productRepository.readPopularSellingProducts(OrderStatus.PAID, startDate, endDate, topFive);
     }
 }
