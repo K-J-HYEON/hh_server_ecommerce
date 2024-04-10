13499b01f4bac473e4c6adf9a6cf565eb936b665
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-10 21:14:01 +0900

    SwaggerConfig 반영

--- src/main/java/hhplus/ecommerce/config/SwaggerConfig.java
+++ src/main/java/hhplus/ecommerce/config/SwaggerConfig.java
@@ -1,38 +1,26 @@
 package hhplus.ecommerce.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-
-import com.fasterxml.classmate.TypeResolver;
-
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-
-@EnableWebMvc
+@OpenAPIDefinition(
+        info = @Info(title = "ecommerce",
+                description = "ecommerce_api 명세서입니다.",
+                version = "v1"))
+@RequiredArgsConstructor
 @Configuration
-public class SwaggerConfig extends WebMvcConfigurationSupport {
-    @Bean
-    public Docket api(TypeResolver typeResolver) {
-        return new Docket(DocumentationType.OAS_30) // 3.0 문서버전으로 세팅
-                .useDefaultResponseMessages(true)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.demo.controller"))
-                .paths(PathSelectors.any())
-                .build();
-    }
+public class SwaggerConfig {
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("ecommerce_api")
-                .description("이커머스 프로젝트 API 명세서입니다.")
-                .version("v1.0")
+    @Bean
+    public GroupedOpenApi SampleOpenApi() {
+        String[] paths = {"/**"};
+
+        return GroupedOpenApi.builder()
+                .group("Sample v1")
+                .pathsToMatch(paths)
                 .build();
     }
 }
