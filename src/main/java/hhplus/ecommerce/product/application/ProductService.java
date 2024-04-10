f2a373ace8e69bf08317fe02805ce6764b9129a1
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-10 21:12:56 +0900

    ProductService 반영

--- src/main/java/hhplus/ecommerce/product/application/ProductService.java
+++ src/main/java/hhplus/ecommerce/product/application/ProductService.java
@@ -1,6 +1,8 @@
 package hhplus.ecommerce.product.application;
 
+import hhplus.ecommerce.config.BaseException;
 import hhplus.ecommerce.product.domain.Product;
+import hhplus.ecommerce.product.dto.res.ProductInfoRes;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,6 +12,5 @@ public interface ProductService {
     List<Product> readProductInfo();
 
     Product readProductInfoDetail(Long productId);
-
-//    ProductInfoRes readPopularProduct(Long productId) throws BaseException;
+    List<Product> readPopularProduct();
 }
