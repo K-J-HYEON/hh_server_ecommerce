d626ec1def965232f95de676bc8fe4713f978c6e
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-11 20:18:47 +0900

    ProductJpaRepository 수정

--- src/main/java/hhplus/ecommerce/product/infrastructure/ProductJpaRepository.java
+++ src/main/java/hhplus/ecommerce/product/infrastructure/ProductJpaRepository.java
@@ -7,21 +7,20 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ProductJpaRepository extends JpaRepository<ProductEntity, Long> {
     List<ProductEntity> findByIdIn(List<Long> productIds);
 
-    @Query("SELECT p FROM ProductEntity p JOIN OrderItemEntity oi ON p.id = oi.productId " +
-            "JOIN OrderEntity oe ON oi.orderId = oi.productId " +
-            "WHERE o.orderStatus = :orderStatus " +
-            "AND o.orderedAt Between :startDate AND :endDate " +
-            "GROUP BY p.productId " +
+    @Query("SELECT pe FROM ProductEntity pe JOIN OrderItemEntity oi ON pe.productId = oi.productId " +
+            "JOIN OrderEntity oe ON oi.orderId = oe.productId " +
+            "WHERE oe.orderStatus = :orderStatus " +
+            "AND oe.orderedAt Between :startDate AND :endDate " +
+            "GROUP BY pe.productId " +
             "ORDER BY SUM(oi.count) DESC")
 
-    Page<ProductEntity> readBestSellingProducts(@Param("orderStatus") OrderStatus orderStatus,
+    Page<ProductEntity> readPopularSellingProducts(@Param("orderStatus") OrderStatus orderStatus,
                                                 @Param("startDate") LocalDateTime startDate,
                                                 @Param("endDate") LocalDateTime endDate,
                                                 Pageable pageable);
