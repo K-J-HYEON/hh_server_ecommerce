4d9d1e24ed2b747cec2ed4f437c0ccb2254d5db6
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-11 20:34:13 +0900

    OrderItemEntity @Table 수정

--- src/main/java/hhplus/ecommerce/orderitem/entity/OrderItemEntity.java
+++ src/main/java/hhplus/ecommerce/orderitem/entity/OrderItemEntity.java
@@ -9,7 +9,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "OrderProductList")
+@Table(name = "OrderItem")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItemEntity extends BaseTimeEntity {
