e71096f34d617e1898c903893a0291e2adbedd8f
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-12 18:38:03 +0900

    dto 패키지 - presentation(controller)패키지 안에 종속

--- src/main/java/hhplus/ecommerce/payment/application/PaymentService.java
+++ src/main/java/hhplus/ecommerce/payment/application/PaymentService.java
@@ -1,7 +1,7 @@
 package hhplus.ecommerce.payment.application;
 
 import hhplus.ecommerce.payment.domain.Payment;
-import hhplus.ecommerce.payment.dto.request.PaymentReq;
+import hhplus.ecommerce.payment.presentation.dto.request.PaymentReq;
 
 public interface PaymentService {
     Payment pay(Long userId, PaymentReq req);
