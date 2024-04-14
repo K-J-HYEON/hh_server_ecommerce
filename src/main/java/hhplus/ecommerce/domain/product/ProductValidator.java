9f733e49b32a74bc3a28efa1ca9a47a5f3fb2013
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-15 00:45:27 +0900

    ProductValidator 반영

---
+++ src/main/java/hhplus/ecommerce/domain/product/ProductValidator.java
@@ -0,0 +1,23 @@
+package hhplus.ecommerce.domain.product;
+
+import hhplus.ecommerce.api.dto.request.OrderRequest;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ProductValidator {
+
+    public void checkProductStockCountForOrder(List<Product> products,
+                                                     List<OrderRequest.ProductOrderRequest> productsOrderRequest) {
+        for (OrderRequest.ProductOrderRequest orderRequest : productsOrderRequest) {
+            Product product = products.stream()
+                    .filter(p -> p.id().equals(orderRequest.id()))
+                    .findFirst()
+                    .orElseThrow(() -> new EntityNotFoundException(orderRequest.id() + " 상품의 정보를 찾지 못했습니다."));
+
+            product.isEnoughProductStockQuantityForOrder(orderRequest.orderCount());
+        }
+    }
+}
