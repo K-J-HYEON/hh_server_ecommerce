d68336c6e3226fb54aa40218fcfb88f4b2148e5c
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-15 00:44:13 +0900

    UserPointController 반영

---
+++ src/main/java/hhplus/ecommerce/api/controller/UserPointController.java
@@ -0,0 +1,35 @@
+package hhplus.ecommerce.api.controller;
+
+import hhplus.ecommerce.api.dto.request.ChargePointRequest;
+import hhplus.ecommerce.api.dto.response.BalanceResponse;
+import hhplus.ecommerce.api.dto.response.ChargePointResponse;
+import hhplus.ecommerce.domain.user.UserPointService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/ecommerce/api/point")
+public class UserPointController {
+
+    private final UserPointService userPointService;
+
+    public UserPointController(UserPointService userPointService) {
+        this.userPointService = userPointService;
+    }
+
+    @Tag(name = "잔액 조회 API", description = "사용자의 잔액 조회 API입니다.")
+    @GetMapping("/{userId}")
+    public BalanceResponse getBalance(@PathVariable Long userId) {
+        Long balance = userPointService.readPoint(userId);
+        return BalanceResponse.from(balance);
+    }
+
+    @Tag(name = "잔액 충전 API", description = "사용자의 잔액을 충전 API입니다.")
+    @PostMapping("/charge/{userId}")
+    public ChargePointResponse charge(@PathVariable Long userId,
+                                      @Valid @RequestBody ChargePointRequest request) {
+        Long balance = userPointService.chargePoint(userId, request.point());
+        return ChargePointResponse.from(balance);
+    }
+}
