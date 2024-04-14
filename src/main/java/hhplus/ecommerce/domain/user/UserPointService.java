86921cc767483487d588da34baa51d03b421ce1d
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-15 00:34:16 +0900

    UserPointService 수정

--- src/main/java/hhplus/ecommerce/domain/user/UserPointService.java
+++ src/main/java/hhplus/ecommerce/domain/user/UserPointService.java
@@ -1,28 +1,23 @@
-package hhplus.ecommerce.user.application;
+package hhplus.ecommerce.domain.user;
 
-import hhplus.ecommerce.user.domain.User;
-import hhplus.ecommerce.user.domain.component.UserReader;
-import hhplus.ecommerce.user.infrastructure.UserPointManager;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserPointServiceImpl implements UserPointService {
+public class UserPointService {
     private final UserReader userReader;
     private final UserPointManager userPointManager;
 
-    public UserPointServiceImpl(UserReader userReader, UserPointManager userPointManager) {
+    public UserPointService(UserReader userReader, UserPointManager userPointManager) {
         this.userReader = userReader;
         this.userPointManager = userPointManager;
     }
 
-    @Override
     public Long chargePoint(Long userId, Long amount) {
         User user = userReader.retrieveByUserId(userId);
         User chargedUser = userPointManager.chargePoint(user, amount);
         return chargedUser.point();
     }
 
-    @Override
     public Long readPoint(Long userId) {
         User user = userReader.retrieveByUserId(userId);
         return user.point();
