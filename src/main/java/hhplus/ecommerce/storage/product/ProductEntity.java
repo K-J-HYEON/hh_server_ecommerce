696620759d5c753cfe1fc5d24f2703c4c103c7b9
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-15 00:47:26 +0900

    ProductEntity 반영

---
+++ src/main/java/hhplus/ecommerce/storage/product/ProductEntity.java
@@ -0,0 +1,54 @@
+package hhplus.ecommerce.storage.product;
+
+import hhplus.ecommerce.config.BaseTimeEntity;
+import hhplus.ecommerce.domain.product.Product;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Product")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProductEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private Long price;
+
+    @Column(name = "stockQuantity")
+    private Long stockQuantity;
+
+    @Column(name = "size")
+    private String size;
+
+    @Column(name = "color")
+    private String color;
+
+    private LocalDateTime createAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createAt;
+    }
+
+    public Product toProduct() {
+        return new Product(getId(), name, price, stockQuantity, size, color);
+    }
+
+    public void updateStock(Long stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+}
