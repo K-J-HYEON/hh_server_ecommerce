1cea02a5e0f7da3257bc11f6b8d55ad7b061477b
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-15 00:32:14 +0900

    OrderController 수정

---
+++ src/main/java/hhplus/ecommerce/api/controller/OrderController.java
@@ -0,0 +1,29 @@
+package hhplus.ecommerce.api.controller;
+
+import hhplus.ecommerce.api.dto.OrderPaidResult;
+import hhplus.ecommerce.api.dto.request.OrderRequest;
+import hhplus.ecommerce.api.dto.response.OrderResponse;
+import hhplus.ecommerce.application.order.OrderUseCase;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/ecommerce/api/order")
+public class OrderController {
+    private final OrderUseCase orderUseCase;
+
+    public OrderController(OrderUseCase orderUseCase) {
+        this.orderUseCase = orderUseCase;
+    }
+
+    @Tag(name = "상품 주문 및 결제 API", description = "상품주문 & 결제 API입니다.")
+    @PostMapping("/{userId}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public OrderResponse order(@PathVariable Long userId,
+                               @Valid @RequestBody OrderRequest request) {
+        OrderPaidResult orderPaidResult = orderUseCase.order(userId, request);
+        return OrderResponse.from(orderPaidResult);
+    }
+}
