28462fd8c3b38160569474cd01f3020773aa3c65
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-15 00:47:55 +0900

    PayType 반영

--- src/main/java/hhplus/ecommerce/storage/payment/PayType.java
+++ src/main/java/hhplus/ecommerce/storage/payment/PayType.java
@@ -1,5 +1,4 @@
-package hhplus.ecommerce.payment.domain.component;
-
+package hhplus.ecommerce.storage.payment;
 
 import lombok.Getter;
 
@@ -8,15 +7,15 @@ import java.util.NoSuchElementException;
 
 @Getter
 public enum PayType {
-    MOBILE_PAY("MOBILE PAY", 1, "모바일 결제"),
+    CARD("CARD", 1, "카드 결제"),
     ACCOUNT_TRANSFER("ACCOUNT_TRANSFER", 2, "계좌 이체"),
-    CARD("CARD", 3, "카드 결제");
+    MOBILE_PAY("MOBILE PAY", 3, "모바일 결제");
 
     private final String method;
-    private final int code;
+    private final Integer code;
     private final String description;
 
-    PayType(String method, int code, String description) {
+    PayType(String method, Integer code, String description) {
         this.method = method;
         this.code = code;
         this.description = description;
@@ -28,8 +27,8 @@ public enum PayType {
 
     public static PayType of(String method) {
         return Arrays.stream(PayType.values())
-                .filter(payType -> payType.method.equals(method))
+                .filter(payType ->  payType.method.equals(method))
                 .findFirst()
-                .orElseThrow(() -> new NoSuchElementException("결제 수단이 존재하지 않습니다."));
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 결제 수단입니다."));
     }
 }
