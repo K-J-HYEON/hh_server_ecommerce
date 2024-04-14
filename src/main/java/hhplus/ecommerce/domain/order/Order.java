8e59f0033d1825bc313e27ff23bbfb20e43bb47d
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-15 00:31:39 +0900

    Order 수정

---
+++ src/main/java/hhplus/ecommerce/domain/order/Order.java
@@ -0,0 +1,19 @@
+package hhplus.ecommerce.domain.order;
+
+import hhplus.ecommerce.storage.order.OrderStatus;
+import java.time.LocalDateTime;
+
+public record Order(
+        Long id,
+        Long userId,
+        Long payAmount,
+        String receiverName,
+        String address,
+        String phoneNumber,
+        String orderStatus,
+        LocalDateTime orderedAt
+) {
+    public Order changeStatus(OrderStatus orderStatus) {
+        return new Order(id, userId, payAmount, receiverName, address, phoneNumber, orderStatus.toString(), orderedAt);
+    }
+}
