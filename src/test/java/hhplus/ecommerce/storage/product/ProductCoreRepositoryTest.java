a47a3fa0353941447056e570101d27345059025d
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-15 00:42:43 +0900

    ProductCoreRepositoryTest 반영

---
+++ src/test/java/hhplus/ecommerce/storage/product/ProductCoreRepositoryTest.java
@@ -0,0 +1,41 @@
+package hhplus.ecommerce.storage.product;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+@SpringBootTest
+class ProductCoreRepositoryTest {
+
+    @Mock
+    private ProductJpaRepository productJpaRepository;
+
+    @InjectMocks
+    private ProductCoreRepository productCoreRepository;
+
+//    @BeforeEach
+//    void setUp() {
+//        productJpaRepository = mock(ProductJpaRepository.class);
+//
+//        productCoreRepository = new ProductCoreRepository(productJpaRepository);
+//    }
+
+    @Test
+    @DisplayName("상품을 찾지 못하였을 경우 에러 발생")
+    void productNotFoundThenError() {
+
+        // Given
+        Long productId = 100L;
+
+        // When & Then
+        assertThrows(EntityNotFoundException.class, () -> {
+            productCoreRepository.findById(productId);
+        });
+    }
+}
