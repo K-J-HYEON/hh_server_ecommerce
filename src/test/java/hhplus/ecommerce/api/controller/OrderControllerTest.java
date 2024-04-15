c4cedf3b8dbbac8cff160a2e8e4c4dc4ee444d52
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-15 21:28:34 +0900

    OrderControllerTest 수정

--- src/test/java/hhplus/ecommerce/api/controller/OrderControllerTest.java
+++ src/test/java/hhplus/ecommerce/api/controller/OrderControllerTest.java
@@ -20,7 +20,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
 @WebMvcTest(OrderController.class)
 class OrderControllerTest {
     @Autowired
