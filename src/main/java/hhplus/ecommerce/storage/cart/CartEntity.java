ebaa5a65f8033933615f0c844dff74502abbc347
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-16 21:47:03 +0900

    CartEntity 수정

---
+++ src/main/java/hhplus/ecommerce/storage/cart/CartEntity.java
@@ -0,0 +1,38 @@
+package hhplus.ecommerce.storage.cart;
+
+import hhplus.ecommerce.config.BaseTimeEntity;
+import hhplus.ecommerce.domain.cart.Cart;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Cart")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CartEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "userId")
+    private Long userId;
+
+//    private LocalDateTime createAt;
+//
+//    private LocalDateTime updateAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public CartEntity(Long userId) {
+        this.userId = userId;
+    }
+
+    public Cart toCart() {
+        return new Cart(getId(), userId);
+    }
+}
