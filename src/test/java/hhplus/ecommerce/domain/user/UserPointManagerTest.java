95ae37ff2e5184c067bf63a23661b7d261b61441
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-17 18:22:56 +0900

    UserPointManagerTest 반영

---
+++ src/test/java/hhplus/ecommerce/domain/user/UserPointManagerTest.java
@@ -0,0 +1,60 @@
+package hhplus.ecommerce.domain.user;
+
+import hhplus.ecommerce.TestFixtures;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+class UserPointManagerTest {
+
+    @MockBean
+    private UserRepository userRepository;
+
+    private UserPointManager userPointManager;
+
+    @BeforeEach
+    void setUp() {
+//        userRepository = mock(UserRepository.class);
+        userPointManager = new UserPointManager(userRepository);
+    }
+
+    @Test
+    @DisplayName("포인트 충전 성공")
+    void charge_point_success() {
+
+        // given
+        User user = TestFixtures.user(1L);
+        Long chargePoint = 100_000L;
+
+        assertThat(user.point()).isEqualTo(10_00_000L);
+
+        given(userRepository.updateUserPoint(any())).willReturn(user.addPoint(chargePoint));
+
+        // when
+        User chargedUser = userPointManager.chargePoint(user, chargePoint);
+
+        // then
+        assertThat(chargedUser.point()).isEqualTo(11_00_000L);
+    }
+
+
+    @Test
+    @DisplayName("포인트 차감 성공")
+    void when_enough_point_use_point_success() {
+        // given
+        User user = TestFixtures.user(1L);
+        Long payAmount = 400_000L;
+
+        given(userRepository.updateUserPoint(any())).willReturn(user.minusPoint(payAmount));
+
+        // when
+        User usePoint = userPointManager.usePoint(user, payAmount);
+
+        // then
+        assertThat(usePoint.point()).isEqualTo(600_000L);
+    }
+}
