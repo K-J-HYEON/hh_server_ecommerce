119181b0de76906360e8186ffbfbfd610d6f42a4
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-20 03:12:14 +0900

    CartCoreRepository 수정

--- src/main/java/hhplus/ecommerce/storage/cart/CartCoreRepository.java
+++ src/main/java/hhplus/ecommerce/storage/cart/CartCoreRepository.java
@@ -1,7 +1,11 @@
 package hhplus.ecommerce.storage.cart;
 
+import hhplus.ecommerce.domain.cart.Cart;
 import hhplus.ecommerce.domain.cart.CartRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public class CartCoreRepository implements CartRepository {
@@ -10,5 +14,15 @@ public class CartCoreRepository implements CartRepository {
 
     public CartCoreRepository(CartJpaRepository cartJpaRepository) {
         this.cartJpaRepository = cartJpaRepository;
+    }
+
+    @Override
+    public Cart findByUserId(Long userId) {
+        Optional<CartEntity> cart = cartJpaRepository.findByUserId(userId);
+        if (cart.isEmpty()) {
+            return cartJpaRepository.save(new CartEntity(userId)).toCart();
+        } else {
+            return cart.get().toCart();
+        }
     }
 }
