60147b97ba9bb1082aff1b17cdb868fa810f1ba6
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-20 03:14:24 +0900

    OrderItemEntity 수정

--- src/main/java/hhplus/ecommerce/storage/orderitem/OrderItemEntity.java
+++ src/main/java/hhplus/ecommerce/storage/orderitem/OrderItemEntity.java
@@ -12,6 +12,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItemEntity extends BaseTimeEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
