018d08f9718f6d8e89282b282eef006919521626
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-22 20:41:02 +0900

    CartItemResponse 반영

---
+++ src/main/java/hhplus/ecommerce/api/dto/response/CartItemResponse.java
@@ -0,0 +1,20 @@
+package hhplus.ecommerce.api.dto.response;
+
+import hhplus.ecommerce.api.dto.CartItemResult;
+import hhplus.ecommerce.api.dto.UnitCartItemResult;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.util.List;
+
+@Schema(description = "장바구니 상품 응답 DTO")
+public record CartItemResponse(List<UnitCartItemResult> cartItems,
+        Long totalPrice) {
+
+    @Schema(description = "장바구니 상품")
+    public static CartItemResponse from(CartItemResult cartItemResult) {
+        return new CartItemResponse(
+                cartItemResult.cartItems(),
+                cartItemResult.totalPrice()
+        );
+    }
+}
