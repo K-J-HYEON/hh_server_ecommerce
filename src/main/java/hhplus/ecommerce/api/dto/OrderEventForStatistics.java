ac4dc27e0e62963b4c0c6768d8f649a3d6c104bc
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-22 20:35:17 +0900

    OrderEventForStatistics.java 삭제

--- src/main/java/hhplus/ecommerce/api/dto/OrderEventForStatistics.java
+++
@@ -1,7 +0,0 @@
-package hhplus.ecommerce.api.dto;
-
-import hhplus.ecommerce.domain.order.Order;
-import hhplus.ecommerce.domain.payment.Payment;
-
-public record OrderEventForStatistics(Order order, Payment payment)  {
-}
