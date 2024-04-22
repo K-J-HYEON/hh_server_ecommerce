1af8d3db5fa3efe7d017fbe1712727a62c7ad67a
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-22 20:11:13 +0900

    UserCoreRepositoryTest 반영

--- src/test/java/hhplus/ecommerce/storage/user/UserCoreRepositoryTest.java
+++ src/test/java/hhplus/ecommerce/storage/user/UserCoreRepositoryTest.java
@@ -5,8 +5,6 @@ import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
