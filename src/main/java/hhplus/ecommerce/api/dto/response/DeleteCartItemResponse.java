bee3966135fdba10e8482ca1fe1c0316df81fe7b
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-22 20:38:36 +0900

    DeleteCartItemResponse 반영

---
+++ src/main/java/hhplus/ecommerce/api/dto/response/DeleteCartItemResponse.java
@@ -0,0 +1,8 @@
+package hhplus.ecommerce.api.dto.response;
+
+public record DeleteCartItemResponse(
+        String message) {
+    public static DeleteCartItemResponse from(String message) {
+        return new DeleteCartItemResponse(message);
+    }
+}
