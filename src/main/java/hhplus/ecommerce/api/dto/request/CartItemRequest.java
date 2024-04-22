a240ad1cad5959365a7bd9ddc26345fa21fa759c
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-22 20:41:11 +0900

    CartItemRequest 반영

---
+++ src/main/java/hhplus/ecommerce/api/dto/request/CartItemRequest.java
@@ -0,0 +1,24 @@
+package hhplus.ecommerce.api.dto.request;
+
+import hhplus.ecommerce.domain.cart.cartitem.NewCartItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public record CartItemRequest(List<CartItem> cartItems) {
+    public record CartItem(Long productId,
+                           Long quantity) {
+    }
+
+    public List<NewCartItem> toNewCartItem() {
+        List<NewCartItem> newCartItems = new ArrayList<>();
+        for (CartItem cartItem : cartItems) {
+            if (cartItem.quantity <= 0) {
+                throw new IllegalArgumentException("0 이하 수량은 장바구니에 추가할 수 없습니다.");
+            }
+
+            newCartItems.add(new NewCartItem(cartItem.productId, cartItem.quantity));
+        }
+        return newCartItems;
+    }
+}
