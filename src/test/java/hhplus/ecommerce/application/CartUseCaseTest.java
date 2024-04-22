c6ba985e260d17525ce142eaf35027c0a91d2b1f
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-22 20:39:03 +0900

    CartUseCaseTest 반영

--- src/test/java/hhplus/ecommerce/application/CartUseCaseTest.java
+++ src/test/java/hhplus/ecommerce/application/CartUseCaseTest.java
@@ -1,33 +1,47 @@
 package hhplus.ecommerce.application;
 
+import hhplus.ecommerce.TestFixtures;
+import hhplus.ecommerce.api.dto.CartItemResult;
+import hhplus.ecommerce.domain.cart.Cart;
 import hhplus.ecommerce.domain.cart.CartService;
+import hhplus.ecommerce.domain.cart.cartitem.CartItem;
 import hhplus.ecommerce.domain.cart.cartitem.NewCartItem;
+import hhplus.ecommerce.domain.product.Product;
 import hhplus.ecommerce.domain.product.ProductService;
+import hhplus.ecommerce.domain.user.User;
 import hhplus.ecommerce.domain.user.UserService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
 import java.util.List;
+
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 class CartUseCaseTest {
 
+    @MockBean
     private UserService userService;
+
+    @MockBean
     private CartService cartService;
+
+    @MockBean
     private ProductService productService;
 
     @InjectMocks
     private CartUseCase cartUseCase;
 
-    @BeforeEach
-    void setUp() {
-        userService = mock(UserService.class);
-        cartService = mock(CartService.class);
-        productService = mock(ProductService.class);
-
-        cartUseCase = new CartUseCase(userService, cartService, productService);
-    }
+//    @BeforeEach
+//    void setUp() {
+//        userService = mock(UserService.class);
+//        cartService = mock(CartService.class);
+//        productService = mock(ProductService.class);
+//
+//        cartUseCase = new CartUseCase(userService, cartService, productService);
+//    }
 
     @Test
     @DisplayName("상품을 장바구니에 추가")
@@ -35,16 +49,76 @@ class CartUseCaseTest {
 
         // given
         Long userId = 1L;
+        Long cartId = 1L;
         Long productId = 1L;
         List<NewCartItem> cartItems = List.of(
                 new NewCartItem(productId, 1L)
         );
 
         // when
-        cartUseCase.addItem(userId, cartItems);
+        cartUseCase.addItem(cartId, userId, cartItems);
 
         // then
         verify(productService, atLeastOnce()).verifyProductStockForAddToCart(any());
         verify(cartService, atLeastOnce()).addItemToCart(any(), anyList());
+    }
+
+    @Test
+    @DisplayName("장바구니 상품 삭제")
+    void deleteCartItems() {
+        // given
+        Long userId = 1L;
+        Long cartId = 1L;
+        List<Long> cartItemIds = List.of();
+
+        User user = TestFixtures.user(userId);
+
+        given(userService.getUser(any())).willReturn(user);
+        given(cartService.getCart(any())).willReturn(new Cart(1L, user.id()));
+        given(cartService.getCartItemsByIds(any(), any())).willReturn(List.of(
+                new CartItem(1L, 1L, 1L, 1L)
+        ));
+
+        // when
+        cartUseCase.deleteItem(cartId, userId, cartItemIds);
+
+        // then
+        verify(cartService, atLeastOnce()).deleteItem(any());
+    }
+
+    @Test
+    @DisplayName("장바구니 추가된 상품 조회")
+    void getCartItems() {
+
+        // given
+        Long cartId = 1L;
+        Long userId = 1L;
+
+        User user = TestFixtures.user(userId);
+
+        Product sample1 = TestFixtures.product("신발");
+        Product sample2 = TestFixtures.product("바지");
+
+        Cart cart = new Cart(1L, userId);
+        List<CartItem> cartItems = List.of(
+                new CartItem(1L, cart.id(), sample1.id(), 1L),
+                new CartItem(1L, cart.id(), sample2.id(), 2L)
+        );
+
+        List<Product> products = List.of(
+                sample1,
+                sample2
+        );
+
+        given(userService.getUser(any())).willReturn(user);
+        given(cartService.getCart(any())).willReturn(cart);
+        given(cartService.getAllCartItems(any())).willReturn(cartItems);
+        given(productService.readProductsByIds(any())).willReturn(products);
+
+        // when
+        CartItemResult foundCartItems = cartUseCase.getCartItems(cartId, userId);
+
+        // then
+
     }
 }
