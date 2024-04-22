e7a86bb855b21de1d18366f4e27084e140629b4b
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-22 20:41:44 +0900

    CartItemJpaRepository 반영

---
+++ src/main/java/hhplus/ecommerce/storage/cart/cartitem/CartItemJpaRepository.java
@@ -0,0 +1,14 @@
+package hhplus.ecommerce.storage.cart.cartitem;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CartItemJpaRepository extends JpaRepository<CartItemEntity, Long> {
+    List<CartItemEntity> findByIdIn(List<Long> ids);
+
+    List<CartItemEntity> findAllByCartId(Long cartId);
+
+    Optional<CartItemEntity> findByCartIdAndProductIdAndDeletedFalse(Long cartId, Long productId);
+}
