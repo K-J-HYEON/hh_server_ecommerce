6af92d82f9cd258abc90fcb85b2d9848a901fec2
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-23 20:14:57 +0900

    ProductCoreRepository 수정

--- src/main/java/hhplus/ecommerce/storage/product/ProductCoreRepository.java
+++ src/main/java/hhplus/ecommerce/storage/product/ProductCoreRepository.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class ProductCoreRepository implements ProductRepository {
@@ -27,10 +28,8 @@ public class ProductCoreRepository implements ProductRepository {
     }
 
     @Override
-    public Product findById(Long productId) {
-        return productJpaRepository.findById(productId)
-                .orElseThrow(() -> new EntityNotFoundException("상품 정보를 찾지 못했습니다. - id: " + productId))
-                .toProduct();
+    public Optional<ProductEntity> findById(Long productId) {
+        return productJpaRepository.findById(productId);
     }
 
     @Override
