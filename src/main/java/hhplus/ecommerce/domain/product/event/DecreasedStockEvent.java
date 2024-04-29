0d631cdea26e74082c434821d0d6327d881f2c71
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-29 09:23:01 +0900

    DecreasedStockEvent 수정

---
+++ src/main/java/hhplus/ecommerce/domain/product/event/DecreasedStockEvent.java
@@ -0,0 +1,18 @@
+package hhplus.ecommerce.domain.product.event;
+
+import hhplus.ecommerce.api.dto.request.OrderRequest;
+import hhplus.ecommerce.domain.order.Order;
+import hhplus.ecommerce.domain.product.Product;
+import hhplus.ecommerce.domain.product.Stock;
+import hhplus.ecommerce.domain.user.User;
+
+import java.util.List;
+
+public record DecreasedStockEvent(
+        User user,
+        Order order,
+        List<Product> products,
+        OrderRequest orderRequest,
+        List<Stock> decreaseStock
+) {
+}
