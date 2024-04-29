1bda2035cc6140b0f91a9c2c9e82cb9018e07383
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-29 09:24:38 +0900

    ProductEventHandler 반영

--- src/main/java/hhplus/ecommerce/api/ProductEventHandler.java
+++ src/main/java/hhplus/ecommerce/api/ProductEventHandler.java
@@ -1,4 +1,25 @@
 package hhplus.ecommerce.api;
 
+
+import hhplus.ecommerce.domain.order.event.OrderPaidEvent;
+import hhplus.ecommerce.domain.product.ProductService;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
 public class ProductEventHandler {
+    private final ProductService productService;
+
+    public ProductEventHandler(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void onProductStockChanged(OrderPaidEvent orderPaidEvent) {
+        productService.updateStockCount(orderPaidEvent.products(), orderPaidEvent.orderRequest().products());
+    }
 }
