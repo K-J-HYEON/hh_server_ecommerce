c03e52399dbee231526affce195e4db5bfded9f0
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-29 19:33:51 +0900

    ChargePointRequest 수정

--- src/main/java/hhplus/ecommerce/api/dto/request/ChargePointRequest.java
+++ src/main/java/hhplus/ecommerce/api/dto/request/ChargePointRequest.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
-
 @Schema(description = "충전 금액 요청 DTO")
 public record ChargePointRequest(
         @NotNull
