6446abbe7296061da8b3be0cc101e42963b40879
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-04-29 09:25:06 +0900

    StockCoreRepository 반영

---
+++ src/main/java/hhplus/ecommerce/storage/product/StockCoreRepository.java
@@ -0,0 +1,32 @@
+package hhplus.ecommerce.storage.product;
+
+import hhplus.ecommerce.domain.product.Stock;
+import hhplus.ecommerce.domain.product.StockRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class StockCoreRepository implements StockRepository {
+
+    private final StockJpaRepository stockJpaRepository;
+
+    public StockCoreRepository(StockJpaRepository stockJpaRepository) {
+        this.stockJpaRepository = stockJpaRepository;
+    }
+
+    @Override
+    public List<Stock> findByProductIdIn(List<Long> productIds) {
+        return stockJpaRepository.findByProductIdIn(productIds)
+                .stream().map(StockEntity::toStock)
+                .toList();
+    }
+
+    @Override
+    public void updateStock(Stock stock) {
+        StockEntity stockEntity = stockJpaRepository.findById(stock.id())
+                .orElseThrow(() -> new EntityNotFoundException("재고 정보를 찾지 못했습니다. - id: " + stock.id()));
+        stockEntity.updateStock(stock.stockCount());
+    }
+}
