2fab466d063e47f351b78edfaaab87201707e797
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-05-02 00:15:24 +0900

    OrderPaidResult 수정

--- src/main/java/hhplus/ecommerce/api/dto/OrderPaidResult.java
+++ src/main/java/hhplus/ecommerce/api/dto/OrderPaidResult.java
@@ -2,31 +2,24 @@ package hhplus.ecommerce.api.dto;
 
 import hhplus.ecommerce.api.dto.request.Receiver;
 import hhplus.ecommerce.domain.order.Order;
-import hhplus.ecommerce.domain.payment.Payment;
 import java.time.LocalDateTime;
 
 public record OrderPaidResult(
         Long orderId,
-        Long paymentId,
         Long payAmount,
         Receiver receiver,
-        String paymentMethod,
-        LocalDateTime orderedAt,
-        LocalDateTime paidAt
+        LocalDateTime orderedAt
 ) {
-    public static OrderPaidResult of(Order order, Payment payment) {
+    public static OrderPaidResult from(Order order) {
         return new OrderPaidResult(
                 order.id(),
-                payment.id(),
-                payment.payAmount(),
+                order.payAmount(),
                 new Receiver(
                         order.receiverName(),
                         order.address(),
                         order.phoneNumber()
                 ),
-                payment.paymentMethod(),
-                order.orderedAt(),
-                payment.paidAt()
+                order.orderedAt()
         );
     }
 }
