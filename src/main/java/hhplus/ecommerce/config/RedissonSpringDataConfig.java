4de56064e3d1f57910a254668fcc01c63295eac2
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-05-04 05:24:19 +0900

    RedissonSpringDataConfig 반영

---
+++ src/main/java/hhplus/ecommerce/config/RedissonSpringDataConfig.java
@@ -0,0 +1,39 @@
+package hhplus.ecommerce.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.redisson.spring.data.connection.RedissonConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
+@Configuration
+public class RedissonSpringDataConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Value("${spring.redis.password}")
+    private String password;
+
+    @Bean
+    public RedissonConnectionFactory redissonConnectionFactory(RedissonClient redisson) {
+        return new RedissonConnectionFactory(redisson);
+    }
+
+    @Bean(destroyMethod = "shutdown")
+    public RedissonClient redisson() throws IOException {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://" + host + ":" + port)
+                .setPassword(password);
+
+        return Redisson.create(config);
+    }
+}
