56b5de5b26a9af3b7138660869bd8cfeb15aca0d
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-05-04 05:24:04 +0900

    OrderCreatedEvent 수정

--- src/main/java/hhplus/ecommerce/domain/order/event/OrderCreatedEvent.java
+++ src/main/java/hhplus/ecommerce/domain/order/event/OrderCreatedEvent.java
@@ -4,14 +4,12 @@ import hhplus.ecommerce.api.dto.request.OrderRequest;
 import hhplus.ecommerce.domain.order.Order;
 import hhplus.ecommerce.domain.payment.Payment;
 import hhplus.ecommerce.domain.product.Product;
-import hhplus.ecommerce.domain.user.User;
-
 import java.util.List;
 
 public record OrderCreatedEvent(
-        User user,
         List<Product> products,
-        OrderRequest orderRequest,
-        Order order
+        List<OrderRequest.ProductOrderRequest> orderRequests,
+        Order order,
+        Payment payment
 ) {
 }
