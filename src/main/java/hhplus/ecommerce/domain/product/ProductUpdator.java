50e8375609959d091360e97a69f8f3248398d282
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-05-10 04:55:28 +0900

    redis반영 - @CachePut 반영

--- src/main/java/hhplus/ecommerce/domain/product/ProductUpdator.java
+++ src/main/java/hhplus/ecommerce/domain/product/ProductUpdator.java
@@ -1,7 +1,9 @@
 package hhplus.ecommerce.domain.product;
 
-import hhplus.ecommerce.api.dto.request.OrderRequest;
+import hhplus.ecommerce.domain.order.Order;
+import hhplus.ecommerce.domain.orderitem.OrderItem;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -14,15 +16,22 @@ public class ProductUpdator {
         this.productRepository = productRepository;
     }
 
-    public void updateStock(List<Product> products, List<OrderRequest.ProductOrderRequest> req) {
-        for (OrderRequest.ProductOrderRequest orderRequest : req) {
+    public void updateStock(List<Product> products, Order order) {
+        for (OrderItem item: order.items()) {
             Product product = products.stream()
-                    .filter(p -> p.id().equals(orderRequest.id()))
+                    .filter(p -> p.id().equals(item.productId()))
                     .findFirst()
-                    .orElseThrow(() -> new EntityNotFoundException(orderRequest.id() + " 상품의 정보를 찾지 못했습니다."));
+                    .orElseThrow(() -> new EntityNotFoundException(item.productId() + " 상품의 정보를 찾지 못했습니다."));
 
-            Product decreasedProduct = product.decreaseStock(orderRequest.orderCount());
+            Product decreasedProduct = product.decreaseStock(item.quantity());
             productRepository.updateStock(decreasedProduct);
         }
     }
+
+    // Cacheable와 비교
+    @CachePut(value = "products", key = "#product.id", unless = "#result == null", cacheManager = "cacheManager", condition = "#caching")
+    public Product updateProductStock(Product product) {
+        return productRepository.updateStock(product);
+    }
+
 }
