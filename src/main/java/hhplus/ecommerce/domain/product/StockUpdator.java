595a69ccc6217e4526579c66a9285395d5744007
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-05-10 05:05:39 +0900

    StockUpdator 수정

--- src/main/java/hhplus/ecommerce/domain/product/StockUpdator.java
+++ src/main/java/hhplus/ecommerce/domain/product/StockUpdator.java
@@ -1,10 +1,7 @@
 package hhplus.ecommerce.domain.product;
 
-import hhplus.ecommerce.api.dto.request.OrderRequest;
-import jakarta.persistence.EntityNotFoundException;
+import hhplus.ecommerce.domain.orderitem.OrderItem;
 import org.springframework.stereotype.Component;
-import java.util.ArrayList;
-import java.util.List;
 
 @Component
 public class StockUpdator {
@@ -15,18 +12,29 @@ public class StockUpdator {
         this.stockRepository = stockRepository;
     }
 
-    public List<Stock> updateStockForOrder(List<Stock> stocks, List<OrderRequest.ProductOrderRequest> products) {
-        List<Stock> stockList = new ArrayList<>();
-        for (OrderRequest.ProductOrderRequest orderRequest : products) {
-            Stock stock = stocks.stream()
-                    .filter(s -> s.productId().equals(orderRequest.id()))
-                    .findFirst()
-                    .orElseThrow(() -> new EntityNotFoundException(orderRequest.id() + " 상품 정보를 찾지 못했습니다."));
-
-            Stock decreaseStock = stock.decreaseStock(orderRequest.orderCount());
-            stockRepository.updateStock(decreaseStock);
-            stockList.add(decreaseStock);
-        }
-        return stocks;
+    public void decreaseStock(Stock stock, OrderItem item) {
+        Stock decreaseStock = stock.decreaseStock(item.quantity());
+        stockRepository.updateStock(decreaseStock);
     }
+
+    public void increaseStock(Stock stock, OrderItem item) {
+        Stock increasedStock = stock.increaseStock(item.quantity());
+        stockRepository.updateStock(increasedStock);
+    }
+
+
+//    public List<Stock> updateStockForOrder(List<Stock> stocks, List<OrderRequest.ProductOrderRequest> products) {
+//        List<Stock> stockList = new ArrayList<>();
+//        for (OrderRequest.ProductOrderRequest orderRequest : products) {
+//            Stock stock = stocks.stream()
+//                    .filter(s -> s.productId().equals(orderRequest.id()))
+//                    .findFirst()
+//                    .orElseThrow(() -> new EntityNotFoundException(orderRequest.id() + " 상품 정보를 찾지 못했습니다."));
+//
+//            Stock decreaseStock = stock.decreaseStock(orderRequest.orderCount());
+//            stockRepository.updateStock(decreaseStock);
+//            stockList.add(decreaseStock);
+//        }
+//        return stocks;
+//    }
 }
