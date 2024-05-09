071cca843b403b54a5fd0a4ccfa129b7fcb3efdc
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-05-10 05:08:41 +0900

    OrderUseCaseTest 수정

--- src/test/java/hhplus/ecommerce/application/OrderUseCaseTest.java
+++ src/test/java/hhplus/ecommerce/application/OrderUseCaseTest.java
@@ -18,9 +18,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.ApplicationEventPublisher;
-
 import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyList;
@@ -82,7 +80,7 @@ class OrderUseCaseTest {
 
         given(userService.getUser(any())).willReturn(user);
         given(productService.readProductsByIds(anyList())).willReturn(products);
-        given(orderService.order(any(), anyList(), any())).willReturn(order);
+//        given(orderService.order(any(), anyList(), any())).willReturn(order);
         given(paymentService.pay(any(), any(), any())).willReturn(payment);
 
         // when
