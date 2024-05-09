239cf1521a81df5b6e1d6e402ef93f67e3635347
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-05-10 05:05:02 +0900

    UserJpaRepository 수정

--- src/main/java/hhplus/ecommerce/storage/user/UserJpaRepository.java
+++ src/main/java/hhplus/ecommerce/storage/user/UserJpaRepository.java
@@ -3,10 +3,11 @@ package hhplus.ecommerce.storage.user;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
-
+import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface UserJpaRepository extends JpaRepository<UserEntity, Long> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
-    Optional<UserEntity> findById(Long userId);
+    @Query("SELECT u FROM UserEntity u WHERE u.id = :userId")
+    Optional<UserEntity> findByIdWithLock(Long userId);
 }
