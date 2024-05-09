5da64cf4e30ae31b5225dba14769e5608cd521f9
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-05-10 05:04:15 +0900

    UserPointService 수정

--- src/main/java/hhplus/ecommerce/domain/user/UserPointService.java
+++ src/main/java/hhplus/ecommerce/domain/user/UserPointService.java
@@ -1,25 +1,39 @@
 package hhplus.ecommerce.domain.user;
 
+import hhplus.ecommerce.common.LockHandler;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserPointService {
     private final UserReader userReader;
     private final UserPointManager userPointManager;
+    private final LockHandler lockhandler;
 
-    public UserPointService(UserReader userReader, UserPointManager userPointManager) {
+    private static final String USER_POINT_LOCK_PREFIX = "USER_";
+
+    public UserPointService(UserReader userReader, UserPointManager userPointManager, LockHandler lockhandler) {
         this.userReader = userReader;
         this.userPointManager = userPointManager;
+        this.lockhandler = lockhandler;
     }
 
+    @Transactional
     public Long chargePoint(Long userId, Long amount) {
-        User user = userReader.retrieveByUserId(userId);
-        User chargedUser = userPointManager.chargePoint(user, amount);
+        String key = USER_POINT_LOCK_PREFIX + userId;
+        lockhandler.lock(key, 2, 1);
+        User chargedUser;
+        try {
+            chargedUser = userPointManager.chargePoint(userId, amount);
+        } finally {
+            lockhandler.unlock(key);
+        }
         return chargedUser.point();
+
     }
 
     public Long readPoint(Long userId) {
-        User user = userReader.retrieveByUserId(userId);
+        User user = userReader.readById(userId);
         return user.point();
     }
 }
