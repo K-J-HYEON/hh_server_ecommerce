c6ec0634fbd5c4f365dab2e1014afb679e8119e7
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-05-10 05:05:14 +0900

    UserEntity 수정

--- src/main/java/hhplus/ecommerce/storage/user/UserEntity.java
+++ src/main/java/hhplus/ecommerce/storage/user/UserEntity.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "User")
+@Table(name = "users")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserEntity extends BaseTimeEntity {
