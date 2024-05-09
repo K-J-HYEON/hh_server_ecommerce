055c9c100701b47ba657099bc87f474e778754fd
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-05-10 05:07:46 +0900

    PaymentService 수정

--- src/main/java/hhplus/ecommerce/domain/payment/PaymentService.java
+++ src/main/java/hhplus/ecommerce/domain/payment/PaymentService.java
@@ -3,42 +3,35 @@ package hhplus.ecommerce.domain.payment;
 import hhplus.ecommerce.api.dto.request.OrderRequest;
 import hhplus.ecommerce.domain.order.Order;
 import hhplus.ecommerce.domain.order.OrderUpdater;
-import hhplus.ecommerce.domain.order.OrderValidator;
 import hhplus.ecommerce.domain.user.User;
 import hhplus.ecommerce.domain.user.UserPointManager;
-import hhplus.ecommerce.domain.user.UserPointValidator;
 import hhplus.ecommerce.storage.order.OrderStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+
 public class PaymentService {
 
     private final OrderUpdater orderUpdater;
-    private final OrderValidator orderValidator;
     private final PaymentAppender paymentAppender;
     private final UserPointManager userPointManager;
-    private final UserPointValidator userPointValidator;
 
-    public PaymentService(OrderUpdater orderUpdater, OrderValidator orderValidator,
+    private static final String USER_POINT_LOCK_PREFIX = "USER_";
+
+    public PaymentService(OrderUpdater orderUpdater,
                           PaymentAppender paymentAppender,
-                          UserPointManager userPointManager,
-                          UserPointValidator userPointValidator) {
+                          UserPointManager userPointManager) {
         this.orderUpdater = orderUpdater;
-        this.orderValidator = orderValidator;
         this.paymentAppender = paymentAppender;
         this.userPointManager = userPointManager;
-        this.userPointValidator = userPointValidator;
     }
 
-    public Payment pay(User user, Order order, OrderRequest request) {
-        if (!orderValidator.isOrderStatusPendingForPay(order)) {
-            throw new IllegalArgumentException("결제 대기 상태가 아닙니다. order status : " + order.orderStatus());
-        }
-
-        userPointValidator.checkUserPointForPay(user, request.paymentAmount());
-        userPointManager.usePoint(user, request.paymentAmount());
-
-        Order paidOrder = orderUpdater.changeStatus(order, OrderStatus.PAID);
-        return paymentAppender.create(paidOrder, request.paymentAmount(), request.paymentMethod());
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public Payment pay(User user, Order order, OrderRequest orderRequest) {
+        userPointManager.usePoint(user, orderRequest.paymentAmount());
+        orderUpdater.changeStatus(order, OrderStatus.PAID);
+        return paymentAppender.create(order, orderRequest.paymentAmount(), orderRequest.paymentMethod());
     }
 }
