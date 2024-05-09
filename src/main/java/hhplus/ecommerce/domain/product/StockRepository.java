965f230e0f88d7057b064c213118d089d544a198
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-05-10 05:03:14 +0900

    StockRepository 수정

--- src/main/java/hhplus/ecommerce/domain/product/StockRepository.java
+++ src/main/java/hhplus/ecommerce/domain/product/StockRepository.java
@@ -3,7 +3,9 @@ package hhplus.ecommerce.domain.product;
 import java.util.List;
 
 public interface StockRepository {
-    List<Stock> findByProductIdIn(List<Long> productIds);
+//    List<Stock> findByProductIdIn(List<Long> productIds);
 
     void updateStock(Stock stock);
+
+    Stock findByProductId(Long productId);
 }
