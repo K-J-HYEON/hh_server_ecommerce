aa01d47132cc92d2685bb689a9c7b091241e60f2
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-05-10 05:07:17 +0900

    ProductEventHandler.java 수정

--- src/main/java/hhplus/ecommerce/api/ProductEventHandler.java
+++ src/main/java/hhplus/ecommerce/api/ProductEventHandler.java
@@ -1,27 +1,27 @@
-package hhplus.ecommerce.api;
-
-
-import hhplus.ecommerce.domain.order.event.OrderPaidEvent;
-import hhplus.ecommerce.domain.product.ProductService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.event.TransactionPhase;
-import org.springframework.transaction.event.TransactionalEventListener;
-
-@Component
-@Slf4j
-public class ProductEventHandler {
-    private final ProductService productService;
-
-    public ProductEventHandler(ProductService productService) {
-        this.productService = productService;
-    }
-
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public void onProductStockUpdate(OrderPaidEvent event) {
-        productService.updateStockCount(event.products(), event.orderRequest().products());
-    }
-}
+//package hhplus.ecommerce.api;
+//
+//
+//import hhplus.ecommerce.domain.order.event.OrderPaidEvent;
+//import hhplus.ecommerce.domain.product.ProductService;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Component;
+//import org.springframework.transaction.annotation.Propagation;
+//import org.springframework.transaction.annotation.Transactional;
+//import org.springframework.transaction.event.TransactionPhase;
+//import org.springframework.transaction.event.TransactionalEventListener;
+//
+//@Component
+//@Slf4j
+//public class ProductEventHandler {
+//    private final ProductService productService;
+//
+//    public ProductEventHandler(ProductService productService) {
+//        this.productService = productService;
+//    }
+//
+//    @Transactional(propagation = Propagation.REQUIRES_NEW)
+//    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+//    public void onProductStockUpdate(OrderPaidEvent event) {
+//        productService.updateStockCount(event.products(), event.orderRequest().products());
+//    }
+//}
