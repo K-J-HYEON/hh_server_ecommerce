e7242d4fb1c91d5396dd5180d4bcea17f7fe4089
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-05-10 05:05:31 +0900

    StockValidator 수정

--- src/main/java/hhplus/ecommerce/domain/product/StockValidator.java
+++ src/main/java/hhplus/ecommerce/domain/product/StockValidator.java
@@ -2,6 +2,7 @@ package hhplus.ecommerce.domain.product;
 
 
 import hhplus.ecommerce.api.dto.request.OrderRequest;
+import hhplus.ecommerce.domain.orderitem.OrderItem;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Component;
 
@@ -10,14 +11,18 @@ import java.util.List;
 @Component
 public class StockValidator {
 
-    public void checkProductStockCountForOrder(List<Stock> stocks, List<OrderRequest.ProductOrderRequest> products) {
-        for (OrderRequest.ProductOrderRequest orderRequest : products) {
-            Stock stock = stocks.stream()
-                    .filter(s -> s.productId().equals(orderRequest.id()))
-                    .findFirst()
-                    .orElseThrow(() -> new EntityNotFoundException(orderRequest.id() + " 상품의 정보를 찾지 못했습니다."));
-
-            stock.isEnoughProductStockCountForOrder(orderRequest.orderCount());
-        }
+    public void checkProductStockCountForOrder(Stock stock, OrderItem item) {
+        stock.isEnoughProductStockCountForOrder(item.quantity());
     }
+
+//    public void checkProductStockCountForOrder(List<Stock> stocks, List<OrderRequest.ProductOrderRequest> products) {
+//        for (OrderRequest.ProductOrderRequest orderRequest : products) {
+//            Stock stock = stocks.stream()
+//                    .filter(s -> s.productId().equals(orderRequest.id()))
+//                    .findFirst()
+//                    .orElseThrow(() -> new EntityNotFoundException(orderRequest.id() + " 상품의 정보를 찾지 못했습니다."));
+//
+//            stock.isEnoughProductStockCountForOrder(orderRequest.orderCount());
+//        }
+//    }
 }
