9f1203ab36f9d2fa79ed6587dbf9f6475d967a23
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-05-16 03:40:33 +0900

    PaymentService 수정

--- src/main/java/hhplus/ecommerce/domain/payment/PaymentService.java
+++ src/main/java/hhplus/ecommerce/domain/payment/PaymentService.java
@@ -11,7 +11,6 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-
 public class PaymentService {
 
     private final OrderUpdater orderUpdater;
