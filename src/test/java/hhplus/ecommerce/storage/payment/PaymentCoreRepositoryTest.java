2d818a288a955dc2ccb4c320a9efcbddb23dbf58
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-05-17 02:00:41 +0900

    PaymentCoreRepositoryTest 수정

--- src/test/java/hhplus/ecommerce/storage/payment/PaymentCoreRepositoryTest.java
+++ src/test/java/hhplus/ecommerce/storage/payment/PaymentCoreRepositoryTest.java
@@ -1,10 +1,8 @@
 package hhplus.ecommerce.storage.payment;
 
-import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 
 class PaymentCoreRepositoryTest {
