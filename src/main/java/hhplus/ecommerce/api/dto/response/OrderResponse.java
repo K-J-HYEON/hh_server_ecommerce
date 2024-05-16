10beaa2709ce2636c27624ba9e130607f30eb89e
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-05-17 02:02:27 +0900

    OrderResponse 수정

--- src/main/java/hhplus/ecommerce/api/dto/response/OrderResponse.java
+++ src/main/java/hhplus/ecommerce/api/dto/response/OrderResponse.java
@@ -2,34 +2,39 @@ package hhplus.ecommerce.api.dto.response;
 
 import hhplus.ecommerce.api.dto.OrderPaidResult;
 import hhplus.ecommerce.api.dto.request.Receiver;
+import hhplus.ecommerce.domain.order.Order;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.format.DateTimeFormatter;
 
 @Schema(description = "주문 응답 DTO")
-public record OrderResponse(
+public record OrderResponse(Long orderId) {
 
-        @Schema(description = "주문 id")
-        Long orderId,
-
-        @Schema(description = "결제 금액")
-        Long payAmount,
-
-        @Schema(description = "받는 사람")
-        Receiver receiver,
-
-        @Schema(description = "주문 날짜")
-        String orderedAt
-) {
-    private final static DateTimeFormatter DATE_TIME_FORMATTER =
-            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
-    public static OrderResponse from(OrderPaidResult orderPaidResult) {
-        return new OrderResponse(
-                orderPaidResult.orderId(),
-                orderPaidResult.payAmount(),
-                orderPaidResult.receiver(),
-                orderPaidResult.orderedAt().format(DATE_TIME_FORMATTER)
-        );
+    public static OrderResponse from(Order order) {
+        return new OrderResponse(order.id());
     }
+
+//        @Schema(description = "주문 id")
+//        Long orderId,
+//
+//        @Schema(description = "결제 금액")
+//        Long payAmount,
+//
+//        @Schema(description = "받는 사람")
+//        Receiver receiver,
+//
+//        @Schema(description = "주문 날짜")
+//        String orderedAt
+//) {
+//    private final static DateTimeFormatter DATE_TIME_FORMATTER =
+//            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+//
+//    public static OrderResponse from(OrderPaidResult orderPaidResult) {
+//        return new OrderResponse(
+//                orderPaidResult.orderId(),
+//                orderPaidResult.payAmount(),
+//                orderPaidResult.receiver(),
+//                orderPaidResult.orderedAt().format(DATE_TIME_FORMATTER)
+//        );
+//    }
 
 }
