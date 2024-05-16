b072ec55cabb3622bf3c2c7a7e1de139ba660248
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-05-17 01:56:14 +0900

    PaymentServiceTest 수정

--- src/test/java/hhplus/ecommerce/domain/payment/PaymentServiceTest.java
+++ src/test/java/hhplus/ecommerce/domain/payment/PaymentServiceTest.java
@@ -3,10 +3,11 @@ package hhplus.ecommerce.domain.payment;
 import hhplus.ecommerce.TestFixtures;
 import hhplus.ecommerce.api.dto.request.OrderRequest;
 import hhplus.ecommerce.api.dto.request.Receiver;
-import hhplus.ecommerce.common.LockHandler;
+import hhplus.ecommerce.config.LockHandler;
 import hhplus.ecommerce.domain.order.Order;
 import hhplus.ecommerce.domain.order.OrderUpdater;
 import hhplus.ecommerce.domain.order.OrderValidator;
+import hhplus.ecommerce.domain.payment.event.PaymentEventPublisher;
 import hhplus.ecommerce.domain.user.User;
 import hhplus.ecommerce.domain.user.UserPointManager;
 import hhplus.ecommerce.domain.user.UserPointValidator;
@@ -15,7 +16,7 @@ import hhplus.ecommerce.storage.payment.PayType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
@@ -25,12 +26,10 @@ import static org.mockito.Mockito.*;
 class PaymentServiceTest {
 
     private OrderUpdater orderUpdater;
-    private OrderValidator orderValidator;
     private PaymentAppender paymentAppender;
     private UserPointManager userPointManager;
-    private UserPointValidator userPointValidator;
     private PaymentService paymentService;
-    private LockHandler lockHandler;
+    private PaymentEventPublisher paymentEventPublisher;
 
     private Long userId;
     private Long orderId;
@@ -41,16 +40,13 @@ class PaymentServiceTest {
     @BeforeEach
     void setUp() {
         orderUpdater = mock(OrderUpdater.class);
-        orderValidator = mock(OrderValidator.class);
         paymentAppender = mock(PaymentAppender.class);
         userPointManager = mock(UserPointManager.class);
-        userPointValidator = mock(UserPointValidator.class);
-        lockHandler = mock(LockHandler.class);
-
+        paymentEventPublisher = mock(PaymentEventPublisher.class);
 
 
         paymentService =
-                new PaymentService(orderUpdater, paymentAppender, userPointManager);
+                new PaymentService(orderUpdater, paymentAppender, userPointManager, paymentEventPublisher);
 
         userId = 1L;
         orderId = 1L;
@@ -77,30 +73,30 @@ class PaymentServiceTest {
         Payment payment = TestFixtures.payment(orderId);
         Order order = TestFixtures.order(OrderStatus.PENDING_FOR_PAY);
 
-        given(orderValidator.isOrderStatusPendingForPay(any())).willReturn(true);
         given(paymentAppender.create(any(), any(), any())).willReturn(payment);
 
         // When
-        Payment paid = paymentService.pay(user, order, request);
+        paymentService.pay(order);
 
         // Then
-        assertThat(paid).isNotNull();
-        assertThat(paid.payAmount()).isEqualTo(500_000L);
-        assertThat(paid.paymentMethod()).isEqualTo("ACCOUNT_TRANSFER");
+//        assertThat(paid).isNotNull();
+//        assertThat(paid.payAmount()).isEqualTo(500_000L);
+//        assertThat(paid.paymentMethod()).isEqualTo("ACCOUNT_TRANSFER");
         verify(userPointManager, atLeastOnce()).usePoint(any(), any());
+        verify(orderUpdater, atLeastOnce()).changeStatus(any(), any());
     }
 
-    @Test
-    @DisplayName("주문 상태가 결제 대기 상태가 아니면 결제 실패")
-    void order_status_not_pending_forpay_then_payment_failed() {
-        // Given
-        Order order = TestFixtures.order(OrderStatus.PAY_FAILED);
-
-        given(orderValidator.isOrderStatusPendingForPay(any())).willReturn(false);
-
-        // When && Then
-        assertThrows(IllegalArgumentException.class, () -> {
-            paymentService.pay(user, order, request);
-        });
-    }
+//    @Test
+//    @DisplayName("주문 상태가 결제 대기 상태가 아니면 결제 실패")
+//    void order_status_not_pending_forpay_then_payment_failed() {
+//        // Given
+//        Order order = TestFixtures.order(OrderStatus.PAY_FAILED);
+//
+////        given(orderValidator.isOrderStatusPendingForPay(any())).willReturn(false);
+//
+//        // When && Then
+//        assertThrows(IllegalArgumentException.class, () -> {
+//            paymentService.pay(order);
+//        });
+//    }
 }
