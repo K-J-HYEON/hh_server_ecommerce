ea3363284a4f0cab3ac61721f38581e615187518
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-05-17 01:55:39 +0900

    PaymentEventPublisher 수정

---
+++ src/main/java/hhplus/ecommerce/domain/payment/event/PaymentEventPublisher.java
@@ -0,0 +1,22 @@
+package hhplus.ecommerce.domain.payment.event;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentEventPublisher {
+    private final ApplicationEventPublisher applicationEventPublisher;
+
+    public PaymentEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+        this.applicationEventPublisher = applicationEventPublisher;
+    }
+
+    public void success(PaymentSuccessEvent event) {
+        applicationEventPublisher.publishEvent(event);
+    }
+
+    public void fail(PaymentFailEvent event) {
+        applicationEventPublisher.publishEvent(event);
+    }
+}
