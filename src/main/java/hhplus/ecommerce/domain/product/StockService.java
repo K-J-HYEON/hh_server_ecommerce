fd968fce0b3f5d6fe786421c84653c3fc9735c82
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-05-17 01:55:49 +0900

    StockService 수정

--- src/main/java/hhplus/ecommerce/domain/product/StockService.java
+++ src/main/java/hhplus/ecommerce/domain/product/StockService.java
@@ -1,8 +1,13 @@
 package hhplus.ecommerce.domain.product;
 
+import hhplus.ecommerce.domain.cart.cartitem.CartItemRemover;
 import hhplus.ecommerce.domain.order.Order;
 import hhplus.ecommerce.domain.order.OrderReader;
+import hhplus.ecommerce.domain.order.OrderUpdater;
 import hhplus.ecommerce.domain.orderitem.OrderItem;
+import hhplus.ecommerce.domain.product.event.StockDecreaseEventFail;
+import hhplus.ecommerce.domain.product.event.StockDecreaseEventSuccess;
+import hhplus.ecommerce.domain.product.event.StockEventPublisher;
 import hhplus.ecommerce.storage.orderitem.OrderItemStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -14,12 +19,33 @@ public class StockService {
     private final StockValidator stockValidator;
     private final StockUpdator stockUpdator;
     private final OrderReader orderReader;
+    private final OrderUpdater orderUpdater;
+    private final CartItemRemover cartItemRemover;
+    private final StockEventPublisher eventPublisher;
 
-    public StockService(StockReader stockReader, StockValidator stockValidator, StockUpdator stockUpdator, OrderReader orderReader) {
+    public StockService(StockReader stockReader, StockValidator stockValidator, StockUpdator stockUpdator, OrderReader orderReader, OrderUpdater orderUpdater, CartItemRemover cartItemRemover, StockEventPublisher eventPublisher) {
         this.stockReader = stockReader;
         this.stockValidator = stockValidator;
         this.stockUpdator = stockUpdator;
         this.orderReader = orderReader;
+        this.orderUpdater = orderUpdater;
+        this.cartItemRemover = cartItemRemover;
+        this.eventPublisher = eventPublisher;
+    }
+
+    @Transactional
+    public void updateStockCountForOrder(Order order) {
+        try {
+            for (OrderItem item : order.items()) {
+                decreaseProductStock(item);
+                orderUpdater.changeItemStatus(item, OrderItemStatus.SUCCESS);
+            }
+            cartItemRemover.resetCart(order.userId());
+            eventPublisher.success(new StockDecreaseEventSuccess(order));
+        } catch (Exception e) {
+            eventPublisher.fail(new StockDecreaseEventFail(order));
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
