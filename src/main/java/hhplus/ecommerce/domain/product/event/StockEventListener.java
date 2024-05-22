9ff0dea002323f03b75d01f9acb671a1eb5714f3
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-05-22 19:43:17 +0900

    StockEventListener 수정

--- src/main/java/hhplus/ecommerce/domain/product/event/StockEventListener.java
+++ src/main/java/hhplus/ecommerce/domain/product/event/StockEventListener.java
@@ -3,7 +3,6 @@ package hhplus.ecommerce.domain.product.event;
 import hhplus.ecommerce.domain.order.OrderService;
 import hhplus.ecommerce.domain.payment.PaymentService;
 import hhplus.ecommerce.domain.product.StockService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
