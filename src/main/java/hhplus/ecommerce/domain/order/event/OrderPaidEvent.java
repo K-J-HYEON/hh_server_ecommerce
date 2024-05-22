c26f960db58db2a71432d503e388dd590062bc1c
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-05-22 19:41:49 +0900

    OrderPaidEvent 수정

--- src/main/java/hhplus/ecommerce/domain/order/event/OrderPaidEvent.java
+++ src/main/java/hhplus/ecommerce/domain/order/event/OrderPaidEvent.java
@@ -4,7 +4,6 @@ import hhplus.ecommerce.api.dto.request.OrderRequest;
 import hhplus.ecommerce.domain.order.Order;
 import hhplus.ecommerce.domain.payment.Payment;
 import hhplus.ecommerce.domain.product.Product;
-
 import java.util.List;
 
 public record OrderPaidEvent(
