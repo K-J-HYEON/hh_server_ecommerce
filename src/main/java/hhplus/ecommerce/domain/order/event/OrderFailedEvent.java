7e2aa596671bb1d6b1c23a43bfc223b289fc369f
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-05-22 19:39:31 +0900

    OrderFailedEvent 수정

--- src/main/java/hhplus/ecommerce/domain/order/event/OrderFailedEvent.java
+++ src/main/java/hhplus/ecommerce/domain/order/event/OrderFailedEvent.java
@@ -4,7 +4,6 @@ import hhplus.ecommerce.api.dto.request.OrderRequest;
 import hhplus.ecommerce.domain.order.Order;
 import hhplus.ecommerce.domain.product.Stock;
 import hhplus.ecommerce.storage.order.OrderStatus;
-
 import java.util.List;
 
 public record OrderFailedEvent(
