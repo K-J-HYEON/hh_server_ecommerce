c462d7396a2ff311ee17f28ea8853e34aed335bf
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-06-05 16:40:23 +0900

    LockHandler 수정

--- src/main/java/hhplus/ecommerce/config/LockHandler.java
+++ src/main/java/hhplus/ecommerce/config/LockHandler.java
@@ -4,12 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Component;
-
 import java.util.concurrent.TimeUnit;
 
 @Component
 @Slf4j
-
 public class LockHandler {
 
     private static final String LOCK_KEY_PREFIX = "LOCK_";
