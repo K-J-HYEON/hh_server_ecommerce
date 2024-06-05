75c854a9039ca93f0b5f426ac8a1d54b4da4b864
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-06-05 16:41:45 +0900

    OrderRequest 수정

--- src/main/java/hhplus/ecommerce/api/dto/request/OrderRequest.java
+++ src/main/java/hhplus/ecommerce/api/dto/request/OrderRequest.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import java.util.List;
 
 @Schema(description = "주문 요청 DTO")
 public record OrderRequest(
