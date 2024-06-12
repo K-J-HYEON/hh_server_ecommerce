70da2b2fddf31a00fca120d583daee6d725adaf2
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-06-12 20:09:47 +0900

    OrderController @RequestMapping 수정

--- src/main/java/hhplus/ecommerce/api/controller/OrderController.java
+++ src/main/java/hhplus/ecommerce/api/controller/OrderController.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/ecommerce/api/order")
+@RequestMapping("/ecommerce/api/orders")
 public class OrderController {
     private final OrderUseCase orderUseCase;
 
