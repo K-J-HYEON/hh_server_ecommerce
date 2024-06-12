34da6b74fbb41d853e029a5c381b9d42ceb34f89
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-06-12 20:10:25 +0900

    ProductRepository 수정

--- src/main/java/hhplus/ecommerce/domain/product/ProductRepository.java
+++ src/main/java/hhplus/ecommerce/domain/product/ProductRepository.java
@@ -1,17 +1,15 @@
 package hhplus.ecommerce.domain.product;
 
 import hhplus.ecommerce.storage.order.OrderStatus;
-import hhplus.ecommerce.storage.product.ProductEntity;
 import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 public interface ProductRepository {
     List<Product> findAll();
 
-    Optional<ProductEntity> findById(Long productId);
+    Product findById(Long productId);
 
     List<Product> findByIdIn(List<Long> productIds);
 
