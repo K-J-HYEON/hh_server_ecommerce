ca17c1757d6eefbe2b6834334fd12bdf6ca3070c
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-06-14 16:57:58 +0900

    ProductCoreRepositoryTest 수정

--- src/test/java/hhplus/ecommerce/storage/product/ProductCoreRepositoryTest.java
+++ src/test/java/hhplus/ecommerce/storage/product/ProductCoreRepositoryTest.java
@@ -1,14 +1,12 @@
 package hhplus.ecommerce.storage.product;
 
 import jakarta.persistence.EntityNotFoundException;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
 
 @SpringBootTest
 class ProductCoreRepositoryTest {
