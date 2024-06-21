df157a73c169c4aa7c56b5f89c66503c800a1f08
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-06-21 23:24:34 +0900

    H2ServerConfig.java 삭제

--- src/main/java/hhplus/ecommerce/api/controller/CartController.java
+++ src/main/java/hhplus/ecommerce/api/controller/CartController.java
@@ -12,7 +12,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
 @RequestMapping("/ecommerce/api/carts")
 public class CartController {
--- src/main/java/hhplus/ecommerce/config/H2ServerConfig.java
+++ src/main/java/hhplus/ecommerce/config/H2ServerConfig.java
@@ -1,6 +1,6 @@
 //package hhplus.ecommerce.config;
 //
-//import org.h2.tools.Server;
+//import io.swagger.v3.oas.models.servers.Server;
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.context.annotation.Profile;
