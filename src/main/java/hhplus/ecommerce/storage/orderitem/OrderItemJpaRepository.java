8aeba15ea9540e6af4cef3fbdacc2449c6cf21d7
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-07-14 20:28:23 +0900

    OrderItemJpaRepository 수정

--- src/main/java/hhplus/ecommerce/storage/orderitem/OrderItemJpaRepository.java
+++ src/main/java/hhplus/ecommerce/storage/orderitem/OrderItemJpaRepository.java
@@ -1,7 +1,6 @@
 package hhplus.ecommerce.storage.orderitem;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface OrderItemJpaRepository extends JpaRepository<OrderItemEntity, Long> {
